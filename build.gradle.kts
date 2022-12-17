6c08be1cec0f804f730a085d6695b941e3d23892
MuV1N <mussenvin3@gmail.com>
2022-12-17 21:47:06 +0100

    Added MariaDB to classpath hopefully MONGODB will work soon

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,14 +12,17 @@ repositories {
 
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-beta.2")
-    implementation("org.mongodb:mongodb-driver-reactivestreams:4.8.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
     implementation("org.slf4j:slf4j-simple:2.0.5")
     implementation("com.google.code.gson:gson:2.10")
     implementation("org.projectlombok:lombok:1.18.24")
-    implementation("org.projectlombok:lombok:1.18.24")
     implementation("io.github.cdimascio:dotenv-java:2.3.1")
     implementation("com.github.twitch4j:twitch4j:1.12.0")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.6")
+    implementation("junit:junit:4.13.2")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.apache.commons:commons-math3:3.6.1")
+
 
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.10")
 }
